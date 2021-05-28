8e0dc089adc93d1f3c6a6502a28104305a39280f
vrindanair89 <vrindanair89@gmail.com>
2021-05-28 21:27:51 +0530

    second

--- school/urls.py
+++ school/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('test1',views.testfunc1,name='test1'),
     path('home',views.dashboard,name='home'),
 ]
--- school/views.py
+++ school/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
+def testfunc1(request):
+    return HttpResponse('hello world')
 def dashboard(request):
     return render(request,'dashboard.html')
