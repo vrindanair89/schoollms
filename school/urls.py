c852613de3403e0ad6ec50811ef044266631ba62
vrindanair89 <vrindanair89@gmail.com>
2021-05-31 20:28:16 +0530

    third

--- school/urls.py
+++ school/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('test1',views.testfunc1,name='test1'),
-    path('home',views.dashboard,name='home'),
+   
+    path('homepage',views.dashboard,name='homepage'),
+    path('events',views.events,name='events'),
+    path('student',views.student,name='student'),
+    path('curriculam',views.curriculam,name='curriculam'),
+    path('faculty',views.faculty,name='faculty'),
+    path('profile',views.profile,name='profile'),
+    path('activities',views.activities,name='activities'),
+    path('login',views.login,name='login'),
+    path('registration',views.register,name='registration'),
+    
+    
 ]
--- school/views.py
+++ school/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def testfunc1(request):
-    return HttpResponse('hello world')
+
 def dashboard(request):
     return render(request,'dashboard.html')
+def events(request):
+    return render(request,'events.html')
+def student(request):
+    return render(request,'student.html')
+def curriculam(request):
+    return render(request,'curriculam.html')
+def faculty(request):
+    return render(request,'faculty.html')
+def profile(request):
+    return render(request,'profile.html')
+def activities(request):
+    return render(request,'activities.html')
+def login(request):
+    return render(request,'login.html')
+def register(request):
+    return render(request,'registration.html')
+
